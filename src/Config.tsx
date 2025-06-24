eed89f4f397dddc640cd765640be4b9fb69af3ec
Tramoreact <163147385+Tramoreact@users.noreply.github.com>
2025-06-24 10:54:29 +0530

    make changes in URL

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,5 +1,11 @@
+// const Config = {
+//   API_URL: 'https://fraud-detection-wv3z.onrender.com',
+// };
+
+// export default Config;
+
 const Config = {
-  API_URL: 'https://fraud-detection-wv3z.onrender.com',
+  API_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
 };
 
 export default Config;
